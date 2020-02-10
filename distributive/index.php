ab79291eb175afefe7e05726ad3f7b93f46ad416
pavelp21 <pavelvp2182@yandex.ru>
2020-02-10 12:50:13 +0500

    Diploma added on Course page

---
+++ distributive/index.php
@@ -0,0 +1,12 @@
+<?php
+$date = strtotime('2020-02-12');
+$now = strtotime('now');
+
+if ($now < $date) {
+    header('location: https://pruffme.com/landing/u8937/tmp1581003881');
+}
+
+else {
+    header('location: https://abramova.mastervision.su/new-system-psychology/course/');
+}
+?>
